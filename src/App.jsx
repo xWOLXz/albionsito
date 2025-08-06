0106f727510b0e7d0c5549879212e50166002ef1
xWOLXz <kvin.vera@gmail.com>
2025-08-06 15:40:19 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.jsx
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import MarketPage from './pages/Market'
-import IndexPage from './pages/Index'
+import MarketPage from './pages/market'
+import IndexPage from './pages/index'
 
 const App = () => {
   return (

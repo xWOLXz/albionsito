8759b97ef7b3a86c4e608c7e4c08c0647204e6fe
xWOLXz <kvin.vera@gmail.com>
2025-08-06 15:10:26 -0500

    Create Loader.jsx

---
+++ components/Loader.jsx
@@ -0,0 +1,8 @@
+// components/Loader.jsx
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center py-10">
+      <img src="/albion-loader.gif" alt="Cargando..." className="w-20 h-20" />
+    </div>
+  );
+}

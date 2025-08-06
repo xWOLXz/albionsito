6c35af97f6594cf38f263a33706d7a11b8272961
xWOLXz <kvin.vera@gmail.com>
2025-08-06 04:19:42 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App.jsx';
+import Market from './pages/market.jsx';
+import Busqueda from './pages/busqueda.jsx';
+import BlackMarket from './pages/blackmarket.jsx';
+import TopGanancias from './pages/top.jsx';
+import Navbar from './components/Navbar.jsx';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Market />} />
+        <Route path="/busqueda" element={<Busqueda />} />
+        <Route path="/blackmarket" element={<BlackMarket />} />
+        <Route path="/top" element={<TopGanancias />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );

57a6305c669357e3648d9b82abebff9b8cb669ec
xWOLXz <kvin.vera@gmail.com>
2025-08-07 17:10:50 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { AlbionProvider } from './context/AlbionContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AlbionProvider>
+      <App />
+    </AlbionProvider>
   </React.StrictMode>
-)
+);

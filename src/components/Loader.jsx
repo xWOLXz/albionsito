1dc784f5e52b86d7ab5f3b0d6f5ec0739459928c
xWOLXz <kvin.vera@gmail.com>
2025-08-07 18:29:38 -0500

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center min-h-[50vh]">
-      <img src="/albion-loader.gif" alt="Cargando..." className="w-24 h-24" />
+    <div style={{textAlign:'center', padding:20}}>
+      <img src="/albion-loader.gif" alt="cargando" style={{width:72}} />
+      <div className="small">Cargando...</div>
     </div>
   );
 }

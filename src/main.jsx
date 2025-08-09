5e9090b8bee92b99ebe1f6e724be9587ea3fea19
xWOLXz <kvin.vera@gmail.com>
2025-08-09 13:58:16 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { AlbionProvider } from './context/AlbionContext';
-import App from './App';   // Aquí tu componente principal con rutas, incluido Market
-
-import './index.css';      // estilos globales
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

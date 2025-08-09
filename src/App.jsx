ab75db28283e815e1584a362980ef6804ed43a07
xWOLXz <kvin.vera@gmail.com>
2025-08-09 14:03:52 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { AlbionProvider } from './context/AlbionContext';
 import Market from './pages/Market';
 
 export default function App() {
-  return (
-    <AlbionProvider>
-      <Market />
-    </AlbionProvider>
-  );
+  return <Market />;
 }

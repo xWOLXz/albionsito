38fdd91435be8917c68c1bffd7e7bf39ee77c324
xWOLXz <kvin.vera@gmail.com>
2025-08-10 11:51:55 -0500

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 export default function Loader() {
   return (
-    <div style={{textAlign:'center', padding:20}}>
-      <img src="/albion-loader.gif" alt="cargando" style={{width:72}} />
+    <div style={{ textAlign: 'center', padding: 20 }}>
+      <img src="/albion-loader.gif" alt="cargando" style={{ width: 72 }} />
       <div className="small">Cargando...</div>
     </div>
   );

ea5249998ad474e999e463715b8f58e066868d27
xWOLXz <kvin.vera@gmail.com>
2025-08-10 09:45:08 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
-import Market from './pages/Market';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AlbionProvider } from "./context/AlbionContext";
+import Market from "./pages/Market";
 
-export default function App() {
-  return <Market />;
+function App() {
+  return (
+    <AlbionProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Market />} />
+        </Routes>
+      </Router>
+    </AlbionProvider>
+  );
 }
+
+export default App;
